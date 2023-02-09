2e72e589fecd2f92c37c47804fe2bd5591ca9446
Brusifi <ba.francoortiz@ugto.mx>
2023-02-09 10:17:45 -0600

    ya acabamos

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'ぼくらの',
+        path: '../music/Song1.mp3',
+        artist: 'Eve',
+        cover: '../images/Cover1.png'
+    },
+    {
+        name: 'ファイトソング',
+        path: '../music/Song2.mp3',
+        artist: 'Eve',
+        cover: '../images/Cover2.png'
+    },
+    {
+        name: 'アウトサイダー',
+        path: '../music/Song3.mp3',
+        artist: 'Eve',
+        cover: '../images/Cover3.png'
+    },
+    {
+        name: 'いのちの食べ方',
+        path: '../music/Song4.mp3',
+        artist: 'Eve',
+        cover: '../images/Cover4.png'
+    },
+    {
+        name: 'お気に召すまま',
+        path: '../music/Song5.mp3',
+        artist: 'Eve',
+        cover: '../images/Cover5.png'
+    }
+]
